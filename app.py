1df1fc48f5464ab369854263e0ce3d30f5b565fb
minhquan-123-hue <time205gon@gmail.com>
2026-06-28 12:14:24 +0700

    28/6 0.3 version, unified UI , jump to result.html after press [submit]

--- app.py
+++ app.py
@@ -1,18 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 import json
 
 app = Flask(__name__)
 
-def read_data():
-    with open("data.json" , "r") as file:
-        data = json.load(file)
+#save data into json
+def store_study_data(hours):
 
-    return data
+    with open("data.json", "w") as file:
+        json.dump(hours, file, indent=4)
 
+
+# check goal
+def match_goal(hours):
+
+    if hours >= 6:
+        return "Good job, keep going"
+    else:
+        return "Need to improve, try harder"
+    
+
+# home page
 @app.route("/")
-def home():
-    hours = read_data()
 
-    return render_template("index.html", hours = hours)
+def home():
+    return render_template("index.html")
+
+
+# receive data from HTML
+@app.route("/submit", methods=["POST"])
+def submit():
+
+    hours = int(request.form["hours"])
+
+    # save
+    store_study_data(hours)
+
+    #calculate messagge
+    message = match_goal(hours)
+
+    return render_template(
+        "result.html",
+        hours=hours,
+        message=message
+    )
 
 app.run(debug=True)
