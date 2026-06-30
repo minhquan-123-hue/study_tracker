4dd30bfe4777c6a772e80db56a542d380f5e315b
minhquan-123-hue <time205gon@gmail.com>
2026-06-30 11:41:57 +0700

    30/6 understand the entire python + flask code do, jump to rewrite to build again for muscle memory

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
 
-import json
+import json # object store info of data structure 
 
-app = Flask(__name__)
+app = Flask(__name__) # this is an object + special variable 
 
-#save data into json
+# save data into json
 def store_study_data(hours):
 
     with open("data.json", "w") as file:
@@ -20,25 +20,33 @@ def match_goal(hours):
         return "Need to improve, try harder"
     
 
-# home page
-@app.route("/")
-
+# home page 
+@app.route("/") 
+# object call method add parameter / 
+# and use decorator to add new functional for the route() method.
 def home():
-    return render_template("index.html")
+    return render_template("index.html") 
 
 
 # receive data from HTML
-@app.route("/submit", methods=["POST"])
+@app.route("/submit", methods=["POST"]) 
+# /submit là nhảy sang một đường dẫn khác khi người dùng gõ vào submit
+# POST là nhận dữ liệu từ browser 
 def submit():
 
-    hours = int(request.form["hours"])
+    hours = int(request.form["hours"]) 
+    # đây là một trường của dữ liệu mà flask nhận lại từ browser
+    # và điền thông tin vào trong đó 
+    # sau đó ta lưu lại sang biến hours 
 
-    # save
+    # save to json file 
     store_study_data(hours)
 
     #calculate messagge
     message = match_goal(hours)
 
+    # hàm render_template của flask
+    # có khả năng đọc các tag và sau đó điền vào thẻ 
     return render_template(
         "result.html",
         hours=hours,
